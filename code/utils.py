0341a2e5f5b2e707883c3d4efecf887d2997b9a6
mjason <mjasoncuba@gmail.com>
2022-11-16 11:15:33 -0500

    Added data downloader

--- code/utils.py
+++ code/utils.py
@@ -1,7 +1,47 @@
+from xmlrpc.client import boolean
 import pandas as pd
+import requests
+import os
+from tqdm import tqdm
 
+def add_color(text):
+	return '\033[91m' + text + '\033[0m'
 
-def data_downloader(csv_path:str):
-    data = pd.read_csv(csv_path)
+def data_images_downloader(csv_path:str, image_folder:str ="images", data_folder:str ="data", force_download=False):
+    '''
+    Download images from a CSV file that provides the URLs to the images.
 
-    print(data)
+    Parameters:
+        csv_path: str   A path to a csv file, where the data of the first column are URLs to images.
+
+        data_folder: str    A path to the directory where the folder to save the images will be created: <data_folder>/<image_folder> 
+        
+        image_folder: str    The folder's name where the images will be saved.
+
+        force_download:     Download the image even if it is already present in the path to be saved.
+    '''
+
+    image_folder = os.path.join(data_folder, image_folder)
+
+    if not os.path.isdir(image_folder):
+        os.mkdir(image_folder)
+
+    data = pd.read_csv(csv_path, header=None)
+
+    print ("# Downloading data from", add_color(csv_path))
+
+    iter = tqdm(range(len(data)))
+    for i in iter:
+        url = str(data.iloc[i, 0]);
+        file_name = url.split('/')[-1]
+        file_path = os.path.join(image_folder,file_name)
+        
+        if os.path.isfile(file_path) and not force_download:
+            continue
+
+        response = requests.get(url)
+        if response.status_code:
+            with open(file_path, 'wb') as fp:
+                fp.write(response.content)
+        else:
+            print (f"\t WARNING: File { file_name } could not be downloaded")
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from code.utils import data_downloader
+from code.utils import data_images_downloader
 
 POSITIVE_EXAMPLES="data/georges.csv"
-NEGATIVE_EXAMPLES="data/no_georges.csv"
+NEGATIVE_EXAMPLES="data/non_georges.csv"
 
 
 def main():
-    data_downloader(POSITIVE_EXAMPLES)
+    data_images_downloader(POSITIVE_EXAMPLES, image_folder="pos", data_folder="data")
+    data_images_downloader(NEGATIVE_EXAMPLES, image_folder="neg", data_folder="data")
 
 if __name__ == '__main__':
     main()
